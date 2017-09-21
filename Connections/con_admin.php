81b1f81e181eaf77dd1e113758bb27e62fec52a9
angel <aandadec@ipn.mx>
2017-09-21 13:06:52 -0500

    respaldo

--- Connections/con_admin.php
+++ Connections/con_admin.php
@@ -2,9 +2,11 @@
 # FileName="Connection_php_mysql.htm"
 # Type="MYSQL"
 # HTTP="true"
-$hostname_con_admin = "localhost";
+$hostname_con_admin = "148.204.14.24";
 $database_con_admin = "paginade_portlog";
 $username_con_admin = "root";
-$password_con_admin = "ye6fczrt3g";
-$con_admin = mysqli_connect($hostname_con_admin, $username_con_admin, $password_con_admin) or die(header('location:../home.php')); 
+$password_con_admin = "origendb";
+#$con_admin = mysqli_connect($hostname_con_admin, $username_con_admin, $password_con_admin) or die(header('location:../home.php')); 
+$con_admin = mysqli_connect("localhost", "root", "origendb", "paginade_portlog") or die(header('location:../home.php')); 
+error_log( mysql_error($con_admin) );
 ?>
--- english/Connections/con_admin.php
+++ english/Connections/con_admin.php
@@ -5,6 +5,6 @@
 $hostname_con_admin = "localhost";
 $database_con_admin = "paginade_port-ingles";
 $username_con_admin = "root";
-$password_con_admin = "ye6fczrt3g";
+$password_con_admin = "origendb";
 $con_admin = mysqli_connect($hostname_con_admin, $username_con_admin, $password_con_admin) or die(header('location:../home.php')); 
 ?>
